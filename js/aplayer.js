3e93a5ac523396acfbd8421fe5b8007198e3ab82
Number-L <2670413692@qq.com>
2024-08-16 14:34:44 +0800

    Site updated: 2024-08-16 14:34:43

---
+++ js/aplayer.js
@@ -0,0 +1,13 @@
+
+const ap = new APlayer({
+  container: document.getElementById("aplayer-main"),
+  fixed: true,
+  audio: [
+    {
+      name: "子夜寄君书",
+      artist: "artist",
+      url: "music/子夜寄君书.mp3",
+      cover: "music/子夜寄君书.jpg",
+    },
+  ],
+});
